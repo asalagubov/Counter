09969dd46d42f6a79b4295d98e31c12dc20e6492
Aleksandr Salagubov <salagubovalex@yandex-team.ru>
2024-01-15 14:40:03 +0300

    Исправлены замечания по проекту Counter

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,63 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class  ViewController: UIViewController {
 
-  var count : Int = 0
-  let date = DateFormatter()
-  var message = String()
+  @IBOutlet private weak var showLable: UILabel! // добавлен лейбл
+  @IBOutlet private weak var addCount: UIButton! // добавлена кнопка +
+  @IBOutlet private weak var deleteCount: UIButton! // добавлена кнопка -
+  @IBOutlet private weak var resetCount: UIButton! // добавлена кнопка Х Reset
+  @IBOutlet private weak var addTextField: UITextView! // добавлен текст лога
 
+  private var count : Int = 0
+  private let date = DateFormatter()
+  private var message = String()
 
-  @IBOutlet weak var isLableCount: UILabel! // добавлен лейбл
-  @IBOutlet weak var isAddCount: UIButton! // добавлена кнопка +
-  @IBOutlet weak var isDeleteCount: UIButton! // добавлена кнопка -
-  @IBOutlet weak var isReset: UIButton! // добавлена кнопка Х Reset
-  @IBOutlet weak var isHistoryCount: UITextView! // добавлен текст лога
-
-  override func viewDidLoad() {
+  override internal func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
 
-    isLableCount.text = "Значение счётчика: \(count)" // делаем значение по умлочанию 0
-    isAddCount.setTitle("+", for: .normal) // делаем значение по умлочанию +
-    isDeleteCount.setTitle("-", for: .normal) // делаем значение по умлочанию -
-    isReset.setTitle(" Reset", for: .normal) // делаем значение по умлочанию Х
-    isHistoryCount.text = "История изменений:" // делаем текст по умолчанию
-    isHistoryCount.isEditable = false // запрет на изменение текста в UITextView
+    showLable.text = "Значение счётчика: \(count)" // делаем значение по умлочанию 0
+    addCount.setTitle("+", for: .normal) // делаем значение по умлочанию +
+    deleteCount.setTitle("-", for: .normal) // делаем значение по умлочанию -
+    resetCount.setTitle(" Reset", for: .normal) // делаем значение по умлочанию Х
+    addTextField.text = "История изменений:" // делаем текст по умолчанию
+    addTextField.isEditable = false // запрет на изменение текста в UITextView
+  }
+  private func historyTextView (_ message: String) {
+    let date = DateFormatter()
+    date.dateFormat = "dd-MM-yyyy HH:mm" // форматируем дату под нужный формат
+    addTextField.text += "\n[\(date.string(from: Date()))] \(message)" // так будет формироваться сообщение в UITextView по нажатию на кнопки
+    let range = NSMakeRange(addTextField.text.count, 1)
+    addTextField.scrollRangeToVisible(range) // добавляем скролл
   }
 
-
-  @IBAction func buttonAddCount(_ sender: Any) {
+  @IBAction private func buttonAddCount(_ sender: Any) {
     print("Тап на кнопку +") // отправляем событие на нажатие + (для статы)
     count += 1 // увеличиваем значение на 1
-    isLableCount.text = String("Значение счётчика: \(count)") // обновляем значение на +1
+    showLable.text = String("Значение счётчика: \(count)") // обновляем значение на +1
     historyTextView("значение изменено на +1")
 
   }
-  @IBAction func buttonDeleteCount(_ sender: Any) {
+  @IBAction private func buttonDeleteCount(_ sender: Any) {
     print("Тап на кнопку -") // отправляем событие на нажатие - (для статы)
     if count == 0 { // ограничиваю min значение 0
-        historyTextView("попытка уменьшить значение счётчика ниже 0") // добавлем текст в UITextView по нажатию на - если значение 0
+      historyTextView("попытка уменьшить значение счётчика ниже 0") // добавлем текст в UITextView по нажатию на - если значение 0
     } else {
       count -= 1
       historyTextView("значение изменено на -1") // добавлем текст в UITextView по нажатию на -
     }
-      isLableCount.text = String("Значение счётчика: \(count)")
-     // обновляем значение на -1, но не ниже 0
+    showLable.text = String("Значение счётчика: \(count)")
+    // обновляем значение на -1, но не ниже 0
   }
-  @IBAction func buttonResetCount(_ sender: Any) {
+  @IBAction private func buttonResetCount(_ sender: Any) {
     print("Тап на кнопку Х") // отправляем событие на нажатие Х (для статы)
     count = 0
-    isLableCount.text = "Значение счётчика: \(count)"  // сбрасываем значение на 0
+    showLable.text = "Значение счётчика: \(count)"  // сбрасываем значение на 0
     historyTextView("значение сброшено") // добавлем текст в UITextView по нажатию Х
 
-  }
-  func historyTextView (_ message: String) {
-    let date = DateFormatter()
-    date.dateFormat = "dd-MM-yyyy HH:mm" // форматируем дату под нужный формат
-    isHistoryCount.text += "\n[\(date.string(from: Date()))] \(message)" // так будет формироваться сообщение в UITextView по нажатию на кнопки
-    let range = NSMakeRange(isHistoryCount.text.count, 1)
-    isHistoryCount.scrollRangeToVisible(range) // добавляем скролл
   }
 }
 
